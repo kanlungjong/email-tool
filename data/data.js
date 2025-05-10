aace8fc6a8bc88ea3e6fb8ad681b7239d2f2a3a9
kanlungjong <kanlungjong@kanlungjongs-MacBook-Pro.local>
2025-05-10 09:23:48 +0700

    Remove web/ from Git tracking

--- data/data.js
+++
@@ -1,7 +0,0 @@
-const userData = {
-    email: "user@gmail.com",
-    profile_picture: "https://images.pexels.com/photos/1264210/pexels-photo-1264210.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-  };
-
-document.getElementById("profile-picture").src = userData.profile_picture;
-document.getElementById("user-email").innerText = userData.email;
